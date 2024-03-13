024c64acb51cd0564c50c42c1c2dd8a083fadad1
paupallares <ppaupallares@gmail.com>
2024-03-13 13:08:27 -0300

    auth0

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,7 @@
+import { auth } from "express-oauth2-jwt-bearer";
+
+export const jwtCheck = auth({
+    audience: process.env.AUTH0_AUDIENCE,
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+    tokenSigningAlg: 'RS256'
+  });
--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,5 +1,4 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
-import { AppState, Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
@@ -16,7 +15,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     throw new Error("no se pudo inicializar auth")
   }
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
+  const onRedirectCallback = () => {
     navigate("/auth-callback")
   };
 
