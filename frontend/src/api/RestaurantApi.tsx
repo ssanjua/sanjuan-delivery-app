29baa7851ce134fcf91bd88dfbf2a4c6c89c893b
paupallares <ppaupallares@gmail.com>
2024-03-18 00:44:46 -0300

    pagination feature

--- frontend/src/api/RestaurantApi.tsx
+++ frontend/src/api/RestaurantApi.tsx
@@ -5,9 +5,11 @@ import { useQuery } from "react-query"
 const API_BASE_URL= import.meta.env.VITE_API_BASE_URL
 
 export const useSearchRestaurants = (searchState: SearchState, city?: string) => {
+
     const createSearchRequest = async() : Promise<RestaurantSearchResponse> => {
         const params = new URLSearchParams()
         params.set("searchQuery", searchState.searchQuery)
+        params.set("page", searchState.page.toString())
 
         const response = await fetch(
             `${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`
--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,4 +1,5 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
+import PaginationSelector from "@/components/PaginationSelector";
 import SearchBar, { SearchForm } from "@/components/SearchBar";
 import SearchResultCard from "@/components/SearchResultCard";
 import SearchResultInfo from "@/components/SearchResultInfo";
@@ -8,20 +9,31 @@ import { useParams } from "react-router-dom";
 
 export type SearchState = {
     searchQuery: string;
+    page: number;
+
 }
 
 const SearchPage = () => {
     const { city } = useParams();
     const [searchState, setSearchState] = useState<SearchState>({
     searchQuery: "",
+    page: 1,
   })
 
   const { results, isLoading } = useSearchRestaurants(searchState, city)
+
+  const setPage = (page: number) => {
+    setSearchState((prevState) => ({
+        ...prevState,
+        page,
+    }))
+  }
 
   const setSearchQuery = (searchFormData: SearchForm) => {
     setSearchState((prevState) => ({
         ...prevState, 
         searchQuery: searchFormData.searchQuery,
+        page: 1,
     }))
   }
 
@@ -29,6 +41,7 @@ const SearchPage = () => {
     setSearchState((prevState) => ({
         ...prevState, 
         searchQuery: "",
+        page: 1,
     }))
   }
   
@@ -55,6 +68,11 @@ const SearchPage = () => {
             {results.data.map((restaurant) => (
                 <SearchResultCard restaurant={restaurant} />
             ))}
+            <PaginationSelector 
+                page={results.pagination.page} 
+                pages={results.pagination.pages} 
+                onPageChange={setPage} 
+            />
         </div>
     </div>
   )
